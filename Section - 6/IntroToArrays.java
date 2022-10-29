b2c14615d03fda1fa802e3e1e32593246ea17954
genericalien8899 <115408845+genericalien8899@users.noreply.github.com>
2022-10-29 20:20:43 +0530

    IntroToArrays
    Key takeaway : The elements inside the arrays are referred using index which starts from 0 to n-1

---
+++ Section - 6/IntroToArrays.java
@@ -0,0 +1,9 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] names = {"Jane","Jill","Joe","Run"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3-1]);
+    }
+}
