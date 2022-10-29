43f103fe677484ca237fd2387c48d646e51029d6
genericalien8899 <115408845+genericalien8899@users.noreply.github.com>
2022-10-29 20:18:20 +0530

    Delete Section 6 directory

--- Section 6/IntroToArrays.java
+++
@@ -1,9 +0,0 @@
-public class IntroToArrays {
-    public static void main(String[] args) {
-        String[] names = {"Jane","Jill","Joe","Run"};
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3-1]);
-    }
-}
